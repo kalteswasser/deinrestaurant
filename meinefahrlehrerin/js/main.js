8adb54c5fdf7b1f343877ab4072e7442c06134cc
kalteswasser <info.kalteswasser@gmail.com>
2022-02-25 11:36:05 +0100

    fahrschulen

---
+++ meinefahrlehrerin/js/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+  
+    /* 1. Visualizing things on Hover - See next part for action on click */
+    $('#stars li').on('mouseover', function(){
+      var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+     
+      // Now highlight all the stars that's not after the current hovered star
+      $(this).parent().children('li.star').each(function(e){
+        if (e < onStar) {
+          $(this).addClass('hover');
+        }
+        else {
+          $(this).removeClass('hover');
+        }
+      });
+      
+    }).on('mouseout', function(){
+      $(this).parent().children('li.star').each(function(e){
+        $(this).removeClass('hover');
+      });
+    });
+    
+    
+    /* 2. Action to perform on click */
+    $('#stars li').on('click', function(){
+      var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+      var stars = $(this).parent().children('li.star');
+      console.log(1);
+      
+      for (i = 0; i < stars.length; i++) {
+        $(stars[i]).removeClass('selected');
+
+      }
+      
+      for (i = 0; i < onStar; i++) {
+        $(stars[i]).addClass('selected');
+      }
+      
+    
+      
+    });
+    
+    
+  });
+  
+  
+  function responseMessage(msg) {
+    $('.success-box').fadeIn(200);  
+    $('.success-box div.text-message').html("<span>" + msg + "</span>");
+    
+
+  }
+
+  // 1-3 Fnc
+  $('.com').click(function(){
+    $('#linkBtn').attr("href","https://form.jotform.com/220552291418351")
+  })
+
+  // 4-5 Fnc
+  $('.net').click(function(){
+    $('#linkBtn').attr("href","happy.html")
+  })
